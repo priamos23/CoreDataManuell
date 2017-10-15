e4ecce8c977ff2781ab6f81e38378c8d9ee08abb
Stefan Vogt <stefan.vogt@priamos.com>
2017-10-15 14:25:45 +0200

    Initial Commit

---
+++ CoreDataManuell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataManuell
+//
+//  Created by Stefan Vogt on 15.10.17.
+//  Copyright © 2017 Priamos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
